325dd2370341267c28a2c9f88e707be506c43072
Vinni <Vinni9@gmail.com>
2024-08-28 20:45:31 -0500

    Primer commit autenticacion

---
+++ src/main/java/co/vinni/Autenticacion/AutenticacionApplication.java
@@ -0,0 +1,13 @@
+package co.vinni.Autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/vinni/Autenticacion/AutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vinni.Autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
