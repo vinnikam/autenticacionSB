93fec2b0eced6bf4094767c1622f97cea2f9cbd7
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2024-08-30 23:09:08 -0500

    Commit : con base de datos Postgres

--- src/main/java/co/vinni/Autenticacion/repositorio/UsuarioRepositorio.java
+++
@@ -1,7 +0,0 @@
-package co.vinni.Autenticacion.repositorio;
-
-import co.vinni.Autenticacion.entidad.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
-}
